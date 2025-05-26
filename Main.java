fc4ff0f543d23525d87cd67a3f9b328499cde9cc
EsmeSnchz08052006 <ms25001@esfe.agape.edu.sv>
2025-05-26 17:04:57 -0600

    Se creo el codigo de la clase Main

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Futbolista jugador = new Futbolista();
+        jugador.nombre = "Messi";
+        jugador.viajar();
+        jugador.concentrarse();
 
+        Entrenador tecnico = new Entrenador();
+        tecnico.nombre = "anchelotti";
+        tecnico.viajar();
+        tecnico.dirigirPartido();
+
+        Masajista masajista = new Masajista();
+        masajista.nombre = "Ronaldo";
+        masajista.viajar();
+        masajista.darMasaje();
+    }
 }
+
